6591191c1647f56188485a6add3ca7ff709485e9
aestrivex <aestrivex@gmail.com>
2015-05-26 19:30:11 -0400

    simple fix for occlusive transparent electrodes in hide noise, same in demo as main unless bug

--- gselu.py
+++ gselu.py
@@ -578,11 +578,13 @@ class ElectrodePositionsModel(HasPrivateTraits):
         self._commit_grid_changes()
 
         if self._noise_hidden:
-            self._update_glyph_lut_event = True
+            #self._update_glyph_lut_event = True
             self._noise_hidden = False
+            self._rebuild_vizpanel_event = True
         else:
-            self._hide_noise_event = True
+            #self._hide_noise_event = True
             self._noise_hidden = True
+            self._rebuild_vizpanel_event = True
 
         self._draw_event = True
 
@@ -1113,21 +1115,22 @@ class SurfaceVisualizerPanel(HasTraits):
 
         self.scene.scene_editor._tool_bar.setVisible(False)
 
-        # we used to snap everything but now we only snap grids
-        # if the user wants to make further changes, they need to snap again
-        unsorted_coordtype = (self._viz_coordtype if 
-            self._viz_coordtype!='snap_coords' else 'surf_coords')
-    
-        unsorted_elecs = map((lambda x:getattr(x, unsorted_coordtype)),
-            self._unsorted_electrodes.values())
-        self.gs_glyphs['unsorted'] = glyph = virtual_points3d( 
-            unsorted_elecs, scale_factor=scale_factor, name='unsorted',
-            figure=self.scene.mayavi_scene, color=self._colors['unsorted'])  
+        #unsorted
+        if not self.model._noise_hidden:
+            unsorted_coordtype = (self._viz_coordtype if 
+                self._viz_coordtype!='snap_coords' else 'surf_coords')
+        
+            unsorted_elecs = map((lambda x:getattr(x, unsorted_coordtype)),
+                self._unsorted_electrodes.values())
+            self.gs_glyphs['unsorted'] = glyph = virtual_points3d( 
+                unsorted_elecs, scale_factor=scale_factor, name='unsorted',
+                figure=self.scene.mayavi_scene, color=self._colors['unsorted'])  
 
-        set_discrete_lut(glyph, self._colors.values())
-        glyph.mlab_source.dataset.point_data.scalars=(
-            np.zeros(len(unsorted_elecs)))
+            set_discrete_lut(glyph, self._colors.values())
+            glyph.mlab_source.dataset.point_data.scalars=(
+                np.zeros(len(unsorted_elecs)))
 
+        #grids
         for i,key in enumerate(self._grids):
             grid_coordtype = (self._viz_coordtype if
                 (self._viz_coordtype!='snap_coords' or
