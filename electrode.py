fix many bugs including offset bug so that all dictionaries now use 'intize' method to store coordinate values as ints which are decimal voxels (generally CT but also RAS) with 4 decimals precision converted to int, also other bugs

electrode.py
@@ -9,6 +9,7 @@ from traitsui.api import (View, Item, HGroup, Handler, CSVListEditor, VGroup,
     MenuBar, Menu)
 from traitsui.message import error as error_dialog
 from utils import ask_user_for_savefile
+from functools import partial
 
 class Electrode(HasTraits):
 #    ct_coords = List(Float)
@@ -73,11 +74,10 @@ class Electrode(HasTraits):
         return nparrayastuple(self.pial_coords)
 
     def asras(self):
-        return tuple(self.surf_coords)
+        return self.surf_coords
 
     def asct(self):
-        return tuple(self.ct_coords)
-
+        return self.ct_coords
 
 def nparrayastuple(nparray):
     nparray = np.array(nparray)

pipeline.py
@@ -1348,8 +1348,8 @@ def snap_electrodes_to_surface(electrodes, subjects_dir=None,
     rh_dura, _ = nib.freesurfer.read_geometry(
         os.path.join(subjects_dir, subject, 'surf', 'rh.dural'))
 
-    lh_dura[:, 0] -= np.max(lh_dura[:, 0])
-    rh_dura[:, 0] -= np.min(rh_dura[:, 0])
+    #lh_dura[:, 0] -= np.max(lh_dura[:, 0])
+    #rh_dura[:, 0] -= np.min(rh_dura[:, 0])
 
     #align the surfaces correctly
     #in the tkRAS space
@@ -1449,8 +1449,8 @@ def snap_electrodes_to_surface(electrodes, subjects_dir=None,
 
 
     #adjust x-axis offsets as pysurfer illogically does as hard-coded step
-    lh_pia[:, 0] -= np.max(lh_pia[:, 0])
-    rh_pia[:, 0] -= np.min(rh_pia[:, 0])
+    #lh_pia[:, 0] -= np.max(lh_pia[:, 0])
+    #rh_pia[:, 0] -= np.min(rh_pia[:, 0])
 
 
     pia = np.vstack((lh_pia, rh_pia))